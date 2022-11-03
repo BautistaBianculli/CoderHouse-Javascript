afe70b49b0c77355280049c081a63dcbf3cd1c31
Bautista Bianculli <bbianculli2000@gmail.com>
2022-11-03 11:44:00 -0300

    Segunda Entrega

--- js/main.js
+++ js/main.js
@@ -1,15 +1,33 @@
 
-let notas = [8,8,9,3,10];
-console.log(PromediarNotas(notas))
+let notas = []
+
+const AgregarNota = () =>{
+    notas.push(parseInt( document.querySelector("#nota").value))
+}
+
  
-function PromediarNotas(array) {
+function PromediarNotas() {
+    let prom = document.querySelector(".promedio")
     let suma = 0;
-    if (array.length === 0){
+    if (notas.length === 0){
         return 0;
     }
-    for(let i = 0; i < array.length; i++) {
-    suma += array[i];
+    notas.forEach(notas => {
+        suma += notas
+    });
+    // for(let i = 0; i < notas.length; i++) {
+    // suma += notas[i];
     
+    // }
+    prom.value = `Promedio : ${ suma / notas.length}`
+   
+}
+
+function BorrarArray(){
+    while(notas.length > 0){
+        notas.shift();
+        console.log(notas.length)
     }
-    return suma / array.length;
+    let prom = document.querySelector(".promedio")
+    prom.value = "No Calculado"
 }
